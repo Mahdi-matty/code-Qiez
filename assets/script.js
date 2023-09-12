3e3babf4986af37f8443e44e3c72a943540ed0bd
unknown <mmiq69@gmail.com>
2023-09-12 13:13:45 -0400

    basic html done and starter js

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,17 @@ startGame.addEventListener("click", function timeRemain() {
 
     }, 1000)
 })
+
+// i have to make test submit to have one correct value and others false?
+formTest = document.querySelector(".testForm")
+let score = 0;
+formTest.addEventListener("submit", function() {
+    if (data-score == true) {
+        score = score + 20;
+    }else {
+        score = score
+    }
+    localStorage.setItem("overAllScore", score);
+})
+
+// i should add eventlistener to start button so when i click test div s start showing one after another.
